5f41dbf2a14475e6355e113334ac840b560ef980
TuanAnhNguyen14111998 <tuananh14111998@gmail.com>
2021-03-23 09:40:28 +0700

    create module crawl image

---
+++ computer_vision/crawl_image/ImageNet-Datasets-Downloader/crawl.sh
@@ -0,0 +1,4 @@
+python ./downloader.py \
+    -data_root ./datasets/ \
+    -number_of_classes 100 \
+    -images_per_class 200
---
+++ computer_vision/crawl_image/OIDv4_ToolKit/download_image.sh
@@ -0,0 +1,1 @@
+python3 main.py downloader --classes Helmet --type_csv train --limit 2000
